3e473c154712e266a3501a5148d0705af96e4569
Noah Hoffman <noah.hoffman@gmail.com>
2025-01-30 10:35:56 -0800

    set XDG variables

--- update-bashrc.bash
+++ update-bashrc.bash
@@ -1,13 +1,24 @@
 #!/usr/bin/bash
 
 if grep -q -s .local/bin/env "$HOME/.bashrc"; then
-   echo "$HOME/.bashrc is up to date"
-   exit
-fi
-
-cat >> "$HOME/.bashrc" <<EOF
+    echo "$HOME/.bashrc already sources \$HOME/.local/bin/env"
+else
+    cat >> "$HOME/.bashrc" <<EOF
 
 . "\$HOME/.local/bin/env"
 EOF
+fi
 
-echo "updated $HOME/.bashrc"
+if grep -q -s XDG "$HOME/.bashrc"; then
+    echo "XDG variables are already configured"
+else
+    cat >> "$HOME/.bashrc" <<EOF
+export XDG_BIN_HOME="\${XDG_BIN_HOME:-\$HOME/.local/bin}"
+export XDG_CACHE_HOME="\${XDG_CACHE_HOME:-\$HOME/.cache}"
+export XDG_CONFIG_HOME="\${XDG_CONFIG_HOME:-\$HOME/.config}"
+export XDG_DATA_HOME="\${XDG_DATA_HOME:-\$HOME/.local/share}"
+EOF
+fi
+
+
+
