385b26b76d178476c6f5725b7e73f8a5e15c1c26
Noah Hoffman <noah.hoffman@gmail.com>
2025-11-25 15:31:32 -0800

    start implementing temp table in query command

--- src/dawgtools/commands/query.py
+++ src/dawgtools/commands/query.py
@@ -15,37 +15,68 @@ For example:
 import argparse
 import csv
 import gzip
-from subprocess import run, CalledProcessError
-import tempfile
-import logging
 import json
+import logging
 
-from jinja2 import Template
-
-from dawgtools.utils import StdOut, MyJSONEncoder
 from dawgtools import db
+from dawgtools.utils import MyJSONEncoder, StdOut
 
 log = logging.getLogger(__name__)
 
 
+def create_and_load_temp_table(sql_cmd: str, fobj: argparse.FileType):
+    """Create and load a temporary table using the provided schema
+    and data files.
+    """
+
+    log.info("Creating temporary table")
+    db.sql_command(sql_cmd)
+
+    log.info("Loading data into temporary table")
+    reader = csv.reader(fobj)
+    headers = next(reader)
+    placeholders = ','.join(['?'] * len(headers))
+    insert_sql = f"insert into temp_table ({', '.join(headers)}) values ({placeholders})"
+    with db.get_connection().cursor() as cur:
+        for row in reader:
+            cur.execute(insert_sql, row)
+        db.get_connection().commit()
+
+
 def build_parser(parser):
-    parser.add_argument('-q', '--query', help="sql command")
-    parser.add_argument('-i', '--infile', type=argparse.FileType('r'),
+
+    inputs = parser.add_argument_group('inputs')
+    inputs.add_argument('-q', '--query', help="sql command")
+    inputs.add_argument('-i', '--infile', type=argparse.FileType('r'),
                         help="Input file containing an sql command")
-    parser.add_argument('-n', '--query-name', choices=db.list_queries(),
+    inputs.add_argument('-n', '--query-name', choices=db.list_queries(),
                         help="name of an sql query")
-    parser.add_argument('-p', '--params', nargs='*',
+    inputs.add_argument('-p', '--params', nargs='*',
                         help="""One or more variable value pairs in
                         the form -e var=val; these are used as
                         parameters when rendering the query.""")
-    parser.add_argument('-P', '--params-file',
+    inputs.add_argument('-P', '--params-file',
                         help="""json file containing parameter values""")
-    parser.add_argument('-o', '--outfile',
-                        help="""Output file name; uses gzip compression
-                        if ends with .gz or stdout if not provided.""")
-    parser.add_argument('-f', '--format', default='lines',
-                        choices=['lines', 'dicts', 'lists'],
-                        help='Output format [%(default)s]')
+
+    temptable = parser.add_argument_group('temptable')
+    temptable.add_argument('--temp-schema', metavar='FILE', type=argparse.FileType('r'),
+                           help="""File containing schema for a temporary
+                           table to be created before running the query.""")
+    temptable.add_argument('--temp-data', metavar='FILE', type=argparse.FileType('r'),
+                           help="""CSV file with columns corresponding
+                           to the schema containing data to load into
+                           the temporary table before running the
+                           query. Requires --temp-schema. Columns not
+                           in the schema are ignored.""")
+
+    outputs = parser.add_argument_group('outputs')
+    outputs.add_argument('-o', '--outfile',
+                         help="""Output file name; uses gzip compression
+                         if ends with .gz or stdout if not provided.""")
+    outputs.add_argument('-f', '--format', default='lines',
+                         choices=['lines', 'dicts', 'lists'],
+                         help='Output format [%(default)s]')
+
     parser.add_argument('-x', '--dry-run', action='store_true', default=False,
                         help='Print the rendered query and exit')
 
@@ -83,7 +114,7 @@ def action(args):
     else:
         outfile = None
         opener = StdOut
-        
+
     with opener(outfile, 'wt', encoding='utf-8', errors='ignore') as f:
         if args.format == 'lines':
             for row in db.as_dicts(headers, rows):
